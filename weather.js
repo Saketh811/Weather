9922e2f69b48e85a4a2b4c2bd41b525555ce68b0
Saketh811 <65438817+Saketh811@users.noreply.github.com>
2021-03-15 09:57:51 +0530

    404 Error detection

--- weather.js
+++ weather.js
@@ -1,6 +1,5 @@
 console.log("Weather function invoked");
-async function getWeather()
-{
+async function getWeather() {
     var key = 'a8cb32ad6930f1fb54ab6bf62c5496c9';
     var city = document.getElementById("city").value;
     var url = "https://api.openweathermap.org/data/2.5/forecast?q="
@@ -10,33 +9,27 @@ async function getWeather()
     var res = document.getElementById("result");
     document.getElementById("table").innerHTML = '<table id = "table"><tr><td><canvas id = "myChart" width = "400" height = "400"></canvas></td></tr></table>';
     var ctx = document.getElementById('myChart').getContext('2d');
-    if(obj.cod=="404")
-    {
+    if (obj.cod == "404") {
         res.innerHTML = "<br>City not found";
         document.getElementById("table").innerHTML = "";
         document.getElementById("time").innerHTML = "";
-    }
-    else if(obj.cod == "400")
-    {
+    } else if (obj.cod == "400") {
         res.innerHTML = "<br>Enter a city";
         document.getElementById("table").innerHTML = "";
         document.getElementById("time").innerHTML = "";
-    }
-    else
-    {
+    } else {
         var country = obj.city.country;
         label = [];
         data = [];
-        var time = obj.list[0].dt_txt.split(" ")[1]; 
-        res.innerHTML = '<p id = "country">'+'Country name: '+country+'</p>'+'<p id = "result">Max Temp is: '+obj.list[0].main.temp_max+'<sup> o</sup>C<br>Min Temp is: '+obj.list[0].main.temp_min+'<sup> o</sup>C</p>';
-        document.getElementById("time").innerHTML = '<p id = "time"> Temperature as of time: '+time+'</p>'
-        for(i=0;i<40;i+=8)
-        {
+        var time = obj.list[2].dt_txt.split(" ")[1];
+        res.innerHTML = '<p id = "country">' + 'Country name: ' + country + '</p>' + '<p id = "result">Max Temp is: ' + obj.list[2].main.temp_max + '<sup> o</sup>C<br>Min Temp is: ' + obj.list[2].main.temp_min + '<sup> o</sup>C</p>';
+        document.getElementById("time").innerHTML = '<p id = "time"> Temperature as of time: ' + time + '</p>'
+        for (i = 0; i < 40; i += 8) {
             var date = obj.list[i].dt_txt.split(" ");
             label.push(date[0]);
             data.push(obj.list[i].main.temp);
         }
-        var myChart = new Chart(ctx,{
+        var myChart = new Chart(ctx, {
             type: 'bar',
             data: {
                 labels: label,
@@ -44,13 +37,13 @@ async function getWeather()
                     label: 'Temperature',
                     data: data,
                     backgroundColor: [
-                        'grey','grey','grey','grey','grey'
+                        'grey', 'grey', 'grey', 'grey', 'grey'
                     ],
                     borderColor: [
-                        'black','black','black','black','black'
+                        'black', 'black', 'black', 'black', 'black'
                     ]
                 }]
             },
         });
-    }   
+    }
 }
